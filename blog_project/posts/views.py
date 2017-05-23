4872a3a421bacf6c3343690266cd19cada4c79af
Dawid <ddawid@windowslive.com>
2017-05-23 20:24:36 +0200

    Create first query set to get all posts

--- blog_project/posts/views.py
+++ blog_project/posts/views.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 
@@ -16,7 +17,10 @@ def post_detail(request):
 
 
 def post_list(request):
+    queryset = Post.objects.all()
+
     context_data = {
+        "object_list": queryset,
         "title": "List"
     }
     return render(request, "index.html", context_data)
